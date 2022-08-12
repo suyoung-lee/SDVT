e061220a843f76ecf42c957dde1a485802634226
root <suyoung.l@kaist.ac.kr>
2022-08-12 17:53:25 +0900

    dummy

--- dummy.py
+++ dummy.py
@@ -1,30 +1,6 @@
 import metaworld
 import random
-'''
-ml10 = metaworld.ML10() # Construct the benchmark, sampling tasks
 
-training_envs = []
-
-
-for name, env_cls in ml10.train_classes.items():
-  env = env_cls()
-  task = random.choice([task for task in ml10.train_tasks
-                        if task.env_name == name])
-
-  print(env)
-  print(task)
-
-  env.set_task(task)
-  training_envs.append(env)
-
-
-for env in training_envs:
-  obs = env.reset()  # Reset environment
-  a = env.action_space.sample()  # Sample an action
-  obs, reward, done, info = env.step(a)  # Step the environoment with the sampled random action
-
-
-'''
 
 ml10 = metaworld.ML10()
 
